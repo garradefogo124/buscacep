0f84ab1c15df9c636da2d500b0ba5628c4c6d436
Micael Miranda <66328408+garradefogo124@users.noreply.github.com>
2022-03-15 16:58:47 -0300

    Add files via upload

---
+++ BuscaCep/pegacep.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuscaCep
+{
+    class pegacep
+    {
+        public String[] pegaDados(String cep)
+        {
+            String[] dados = { "", "", "", "", "", "" };
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create
+                ("https://viacep.com.br/ws/" + cep + "/json/");
+            request.AllowAutoRedirect = false;
+            HttpWebResponse ChecaServidor = (HttpWebResponse)request.GetResponse();
+
+            if (ChecaServidor.StatusCode != HttpStatusCode.OK)
+            {
+                MessageBox.Show("Servidor indisponível");
+            }
+
+            using (Stream webStream = ChecaServidor.GetResponseStream())
+            {
+                if (webStream != null)
+                {
+                    using (StreamReader responseReader = new StreamReader(webStream))
+                    {
+                        string response = responseReader.ReadToEnd();
+                        response = Regex.Replace(response, "[{},]", string.Empty);
+                        response = response.Replace("\"", "");
+
+                        String[] substrings = response.Split('\n');
+
+                        int cont = 0;
+                        foreach (var substring in substrings)
+                        {
+                            if (cont == 1)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+
+                                if (valor[0] == "  erro")
+                                {
+                                    dados[0] = "NÃO ENCONTRADO";
+                                    return dados;
+                                }
+                            }
+
+                            //Logradouro
+                            if (cont == 2)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                dados[1] = valor[1];
+                            }
+
+                            //Complemento
+                            if (cont == 3)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                dados[2] = valor[1];
+                            }
+
+                            //Bairro
+                            if (cont == 4)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                dados[3] = valor[1];
+                            }
+
+                            //Localidade (Cidade)
+                            if (cont == 5)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                dados[4] = valor[1];
+                            }
+
+                            //Estado (UF)
+                            if (cont == 6)
+                            {
+                                string[] valor = substring.Split(":".ToCharArray());
+                                dados[5] = valor[1];
+                            }
+
+                            cont++;
+                        }
+                    }
+                }
+            }
+
+            return dados;
+        }
+    }
+}
